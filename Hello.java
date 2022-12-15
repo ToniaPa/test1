0443550daf0793fdc489e258b8f2617ce6c15456
Tonia Papadogiorgaki <papadogiorgaki@outlook.com>
2022-12-15 19:58:06 +0200

    Hello v0.3

--- Hello.java
+++ Hello.java
@@ -1,38 +1,5 @@
 public class Hello{
 	public static void main(String... args){
-		System.out.println("hi");	
+		System.out.println("Hello");	
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
